c9de669f5ce1f3c63f1769873191955ecd26408e
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-05-04 22:21:20 +0200

    added a sample rt from the docs

--- main.tf
+++ main.tf
@@ -24,6 +24,24 @@ resource "aws_subnet" "subnets" {
   tags = var.resource_tags
 }
 
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
