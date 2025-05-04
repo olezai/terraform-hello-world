f99ac1c5e189c423fdb177804b729aa84da69dd7
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-05-04 17:42:45 +0200

    added new subnets and attached to ec2

--- main.tf
+++ main.tf
@@ -6,6 +6,38 @@ resource "aws_vpc" "main" {
   tags = var.resource_tags
 }
 
+resource "aws_subnet" "my_public_subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.100.0.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = var.resource_tags
+}
+
+resource "aws_subnet" "my_public_subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.100.1.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = var.resource_tags
+}
+
+resource "aws_subnet" "my_private_subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.100.10.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = var.resource_tags
+}
+
+resource "aws_subnet" "my_private_subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.100.11.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = var.resource_tags
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -27,11 +59,12 @@ resource "random_pet" "name" {}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
+  subnet_id     = aws_subnet.my_public_subnet1.id
 
-    tags = merge(
+  tags = merge(
     var.resource_tags,
     {
-        Name = random_pet.name.id
+      Name = random_pet.name.id
     }
-    )
+  )
 }
