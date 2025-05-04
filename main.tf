4a95aeedcff8a6d7b3815a90ed3f973952f1b9c9
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-05-04 23:20:16 +0200

    allow any to ec2: ping, ssh, http

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "allow_http" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   security_group_id = aws_security_group.allow_http.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -126,7 +126,7 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -151,7 +151,7 @@ resource "aws_security_group" "allow_icmp" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_icmp_ipv4" {
   security_group_id = aws_security_group.allow_icmp.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = -1
   ip_protocol       = "icmp"
   to_port           = -1
